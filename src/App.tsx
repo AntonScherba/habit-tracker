5cc9813f5bb41c518d40a5fed3650afd9f221b80
Anton Shcherba <anton@vergelijkdirect.com>
2022-11-06 20:47:04 +0100

    added lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import './App.css';
 import Dialog from './Dialog';
 
 function App() {
+  const a = 0;
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Dialog></Dialog>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+      dolor facere eligendi nam et magnam ad iste facilis, quae laboriosam
+      officia officiis eius sapiente, deleniti adipisci fugit? Perspiciatis,
+      placeat quisquam!
     </div>
   );
 }
