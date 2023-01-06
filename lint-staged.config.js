fcc1e0b2a50ba6517590f40c3093166b2e6ebeb9
Anton Shcherba <anton@vergelijkdirect.com>
2023-01-06 18:11:33 +0100

    4 changed eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx,md,html}': ['eslint --max-warnings=0', 'prettier --write'],
-  '*.{css, json}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx,html}': ['eslint --max-warnings=0', 'prettier --write'],
+  '*.{css,md,json}': ['prettier --write'],
 };
