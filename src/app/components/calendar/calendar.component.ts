252cfcc44c524ae0eee51d981c052b37f2f34452
Anton Shcherba <anton@vergelijkdirect.com>
2024-09-22 13:35:03 +0200

    master - update calendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -14,6 +14,7 @@ import {
   CdkVirtualScrollViewport,
   ScrollingModule,
 } from '@angular/cdk/scrolling';
+import { delay, take } from 'rxjs';
 
 const OFFSET = 3;
 
@@ -29,41 +30,55 @@ export class CalendarComponent implements OnInit, AfterViewInit {
   calendarService = inject(CalendarService);
 
   calendar: Dayjs[] = [];
+  initialized = false;
 
   @ViewChild(CdkVirtualScrollViewport) viewPort!: CdkVirtualScrollViewport;
 
   ngOnInit() {
-    this.generateCalendar(this.calendarService.date.value);
+    this.#generateCalendar(this.calendarService.date.value);
   }
 
   ngAfterViewInit() {
     this.viewPort.scrolledIndexChange
-      .pipe
-      // delay(0),
-      // take(1)
-      ()
-      .subscribe((index) => {
-        // const i = this.calendar.findIndex((date) => date.isSame(this.calendarService.currentDate));
-        // this.viewPort?.scrollToIndex(Math.max(i - OFFSET, 0), 'instant');
-
-        this.calendarService.setDate(this.calendar[index + OFFSET]);
-      });
-
-    // this.viewPort.scrolledIndexChange
-    //   .subscribe((index) => {
-    //     this.calendarService.setDate(this.calendar[index+OFFSET]);
-    //   });
+      .pipe(delay(0), take(1))
+      .subscribe(() => this.#waitScrolledChange());
   }
 
-  generateCalendar(date: Dayjs) {
+  #waitScrolledChange() {
+    const index = this.calendar.findIndex((date) =>
+      date.isSame(this.calendarService.currentDate),
+    );
+    this.scrollToDate(index, 'instant');
+  }
+
+  #getDateByIndex(index: number) {
+    return this.calendar[index];
+  }
+
+  #generateCalendar(date: Dayjs) {
     const daysInMonth = date.daysInMonth();
     const days = range(1, daysInMonth);
-
     this.calendar = days.map((day) => date.clone().set('D', day));
   }
 
-  onDayChange(date: Dayjs, index: number) {
+  onDayScroll(index: number) {
+    const date = this.#getDateByIndex(index + OFFSET);
     this.calendarService.setDate(date);
-    this.viewPort?.scrollToIndex(Math.max(index - OFFSET, 0), 'smooth');
+  }
+
+  onDayClick(date: Dayjs, index: number) {
+    this.calendarService.setDate(date);
+    this.scrollToDate(index);
+  }
+
+  scrollToDate(index: number, behavior: ScrollBehavior = 'smooth') {
+    this.viewPort?.scrollToIndex(Math.max(index - OFFSET, 0), behavior);
+  }
+
+  get isDisabled() {
+    const index = this.calendar.findIndex((date) =>
+      this.calendarService.date.value.isSame(date),
+    );
+    return this.calendar.slice(index + 1).length <= 3;
   }
 }
