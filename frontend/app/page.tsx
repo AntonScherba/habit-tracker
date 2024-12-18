30cd839d0a3775105750d14498e37d3f641331dd
AntonScherba <antonScherba95@gmail.com>
2024-12-18 19:50:39 +0100

    master - update frontend page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Just do it!!!</h1>
+        <h1>Updated Just do it!!!</h1>
       </main>
     </div>
   );
